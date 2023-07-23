afb5119a629c45670a6084d953df95262fd8b4b8
koker007 <55190926+koker007@users.noreply.github.com>
2023-07-23 22:53:55 +0500

    Testing Planet Concept

--- Assets/Scripts/Tests/TestMovePlanet/TestMovePlanet.cs
+++ Assets/Scripts/Tests/TestMovePlanet/TestMovePlanet.cs
@@ -10,6 +10,20 @@ public class TestMovePlanet : MonoBehaviour
     GameObject player;
     [SerializeField]
     GameObject SkyBox;
+    [SerializeField]
+    Material shadowMat;
+
+    [SerializeField]
+    float GradX = 180;
+    [SerializeField]
+    float GradZ = 360;
+
+    Vector3 playerMoveVector = new Vector3(1, 1, 1);
+
+    Texture2D shadowTexture;
+    [SerializeField]
+    //положение солнца
+    Vector2 sunPos = new Vector2(0.0f, 0.5f);
 
     void Start()
     {
@@ -23,17 +37,23 @@ public class TestMovePlanet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        RotateSun();
+        TestSun();
+
+        
+
         MovePlayer();
         RotateSkyBox();
+        GenSunTexture();
     }
 
     void MovePlayer() {
         if (Input.GetKey(KeyCode.Keypad8)) {
-            player.transform.localPosition = new Vector3(player.transform.localPosition.x, 0, player.transform.localPosition.z + Time.unscaledDeltaTime);
+            player.transform.localPosition = new Vector3(player.transform.localPosition.x, 0, player.transform.localPosition.z + Time.unscaledDeltaTime * playerMoveVector.z);
         }
         if (Input.GetKey(KeyCode.Keypad5))
         {
-            player.transform.localPosition = new Vector3(player.transform.localPosition.x, 0, player.transform.localPosition.z - Time.unscaledDeltaTime);
+            player.transform.localPosition = new Vector3(player.transform.localPosition.x, 0, player.transform.localPosition.z - Time.unscaledDeltaTime * playerMoveVector.z);
         }
         if (Input.GetKey(KeyCode.Keypad6))
         {
@@ -46,15 +66,35 @@ public class TestMovePlanet : MonoBehaviour
 
         Vector3 pos = player.transform.localPosition;
 
-        if (pos.x > planet.transform.localScale.x)
-            pos.x -= planet.transform.localScale.x;
-        else if (pos.x < 0)
-            pos.x += planet.transform.localScale.x;
-
+        //Вертикальное движение
         if (pos.z > planet.transform.localScale.z)
-            pos.z -= planet.transform.localScale.z;
+        {
+            float offset = planet.transform.localScale.z - pos.z;
+
+            pos.z = planet.transform.localScale.z - offset;
+            pos.x += planet.transform.localScale.x / 2;
+            //Меняем направление движения
+            playerMoveVector.z *= -1;
+        }
         else if (pos.z < 0)
-            pos.z += planet.transform.localScale.z;
+        {
+            float offset = pos.z * -1;
+            pos.z += offset;
+            pos.x += planet.transform.localScale.x / 2;
+
+            //Меняем направление движения
+            playerMoveVector.z *= -1;
+        }
+
+        //горизонтальное движение
+        if (pos.x > planet.transform.localScale.x)
+        {
+            pos.x -= planet.transform.localScale.x;
+        }
+        else if (pos.x < 0)
+        {
+            pos.x += planet.transform.localScale.x;
+        }
 
         player.transform.localPosition = pos;
     }
@@ -68,13 +108,103 @@ public class TestMovePlanet : MonoBehaviour
         float coofX = playerPos.x / planetScale.x;
         float coofZ = playerPos.z / planetScale.z;
 
-        //Вычисляем градусы по x
-        float rotX = 360 * coofZ;
-        float rotZ = 360 * coofX;
+        float rotX = 0;
+        float rotZ = 0;
+        float rotY = 0;
 
-        float rotY = 360 * coofZ;
+        //Вычисляем градусы по x
+        //rotX = GradX * coofZ;
+        //rotZ = GradZ * coofX + GradX * coofZ;
+
+        //rotY = GradX * coofZ;
+
+        rotX = -GradX * coofZ + GradX/2;
+        rotZ = GradZ * coofX;
+
+        //rotY = GradX * coofZ;
 
         Quaternion rot = Quaternion.Euler(rotX, rotY, rotZ);
         SkyBox.transform.rotation = rot;
+    }
+
+    void TestSun() {
+        if (sunPos.x < 0)
+            sunPos.x += 1.0f;
+        else if(sunPos.x > 1.0f)
+            sunPos.x -= 1.0f;
+
+        if (sunPos.y < 0)
+            sunPos.y = 0;
+        else if (sunPos.y > 1)
+            sunPos.y = 1;
+    }
+    void RotateSun()
+    {
+
+        sunPos.x = (Time.unscaledTime / 10)%1.0f;
+
+        sunPos.y = (Mathf.Sin(Time.unscaledTime / 30)/2)+0.5f;
+
+    }
+
+    void GenSunTexture() {
+        shadowTexture ??= new Texture2D(200, 100);
+
+        //Проходимся по каждому пикселю
+        for (int pixX = 0; pixX < shadowTexture.width; pixX++) {
+            float pixCoofX = (float)pixX / (float)shadowTexture.width;
+
+            for (int pixY = 0; pixY < shadowTexture.height; pixY++) {
+                float pixCoofY = (float)pixY / (float)shadowTexture.height;
+
+                //Вычисляем освещенность пикселя
+                //Если коофицент больше 0.5 то это темная сторона солнца
+                float lightingCoof = 0;
+
+                float lightX = 1 - Mathf.Abs(sunPos.x - pixCoofX);
+                float lightXM = 1 - Mathf.Abs(sunPos.x - (pixCoofX - 1));
+                float lightXP = 1 - Mathf.Abs(sunPos.x - (pixCoofX + 1));
+
+                if (lightX < lightXM)
+                    lightX = lightXM;
+                if (lightX < lightXP)
+                    lightX = lightXP;
+
+                lightingCoof = lightX;
+
+                float lightY = 1 - Mathf.Abs(sunPos.y - pixCoofY);
+                lightY += 0.25f;
+
+                lightingCoof = lightX;
+                if (lightY < lightX)
+                    lightingCoof = lightY;
+
+                //Узнаем освещение за пределами полюсов
+                float lightYP = 1 - Mathf.Abs(sunPos.y - (1 + (1 - pixCoofY)));
+                float lightYM = 1 - Mathf.Abs(sunPos.y - pixCoofY * -1);
+                lightYP += 0.25f;
+                lightYM += 0.25f;
+
+                if (lightingCoof < lightYM)
+                    lightingCoof = lightYM;
+                if (lightingCoof < lightYP)
+                    lightingCoof = lightYP;
+
+
+                lightingCoof = lightingCoof - 0.7f;
+                lightingCoof *= 5;
+
+                lightingCoof = 1 - lightingCoof;
+
+                shadowTexture.SetPixel(pixX, pixY, new Color(0, 0, 0, lightingCoof));
+
+            }
+        }
+
+        shadowTexture.Apply();
+        shadowTexture.filterMode = FilterMode.Point;
+
+        shadowMat.mainTexture = shadowTexture;
+        
     }
 }
